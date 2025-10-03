dc9c3f88ddfdf79a5493dd094b86b040e7cd7a56
Joshua Peper <joshua@envitron.com>
2025-10-03 15:49:56 +0200

    Publish config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path for GitHub Pages project sites. Override by setting GHP_BASE, e.g. "/where4oclock/"
-  base: process.env.GHP_BASE || '/',
+  base: '/beer-o-clock-globe/',
 })
